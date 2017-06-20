f0c909a9d8d6ebcad2a2417d56992315c941ab35
nkls <niklas@long.ch>
2017-06-19 19:47:51 -0700

    add import for calc module

---
+++ src/app/calc.js
@@ -0,0 +1,191 @@
+var titanicData;
+
+
+export var findAgeRange = function() {
+  let ageRange = {max: -1, min: 1000};
+
+  for (let i in titanicData) {
+    if (!titanicData.hasOwnProperty(i)) {continue;}
+
+    let personAge = parseFloat(titanicData[i].age);
+
+    if(personAge < ageRange.min) {
+      ageRange.min = personAge;
+    }
+
+    if(personAge > ageRange.max) {
+      ageRange.max = personAge;
+    }
+  }
+  console.log(ageRange);
+  return ageRange;
+};
+
+
+export var breakdownAgeCount = function() {
+  let ages = {};
+
+  for (let i in titanicData) {
+    if (!titanicData.hasOwnProperty(i)) {continue;}
+
+    let personAge = parseInt(titanicData[i].age);
+
+    if (ages[personAge] === undefined) {
+      ages[personAge] = 1;
+    } else {
+      ages[personAge] += 1;
+    }
+
+  }
+  console.log(ages);
+  return ages;
+};
+
+
+export var excludeNaN = function(ageBreakdown) {
+  console.log(ageBreakdown);
+  delete ageBreakdown[NaN];
+
+  let formattedData = [];
+  
+  for (let i in ageBreakdown) {
+    if (!ageBreakdown.hasOwnProperty(i)) continue;
+
+    formattedData.push({age: i, count: ageBreakdown[i]});
+  }
+  return formattedData;
+};
+
+
+export var countTotalAgesNum = function(ageCount) { //counts number of ages
+  ageNum = null;
+
+  for (let i in ageCount) {
+    if (!ageCount.hasOwnProperty(i)) {continue;}
+
+    ageNum += ageCount[i];
+  }
+  return ageNum;
+};
+
+
+export var ageByPercentage = function(ageBreakdown, totalAgeCount) {
+  let agePercentages = {};
+
+  for (let i in ageBreakdown) {
+    if (!ageBreakdown.hasOwnProperty(i)) {continue;}
+
+    console.log(ageBreakdown[i]);
+    agePercentages[i] = (ageBreakdown[i] * 100 / totalAgeCount);
+
+  }
+
+  //verification
+
+  let totalPercentage = null;
+
+  for (let i in agePercentages) {
+    totalPercentage += agePercentages[i];
+  }
+
+  // console.log(agePercentages);
+  // console.log(totalPercentage);
+  return agePercentages;    
+};
+
+
+export var ageRangePercentage = function(min, max, agePercentages) {
+  let percentage = 0; 
+
+  for (let i in agePercentages) {
+    if (!agePercentages.hasOwnProperty(i)) {continue;}
+
+    if (i >= min && i <= max && isNaN(i) === false) {
+      percentage += agePercentages[i];
+    }
+  }
+
+  return percentage;
+};
+
+
+
+//D3 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+export var draw = function(data) {
+
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+
+  // set the ranges
+
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+  // define the line
+
+
+
+  // append the svg object to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // Get the data
+  // d3.csv("titanic3.csv", function(error, data) {
+  //   if (error) throw error;
+
+    console.log(data);
+    
+    // format the data
+    data.forEach(function(d) {
+        d.age = +d.age; // formats whatever d.age is in d3.csv to number
+        d.count = +d.count;
+    });
+
+    // scale the range of the data
+    // d3.extent([1, 4, 3, 2]) -> [1, 4]
+    x.domain(d3.extent(data, function(d) { return d.age; }));
+    y.domain(d3.extent(data, function(d) { return d.count; }));
+
+    // add the dots
+    svg.selectAll("dot")
+      .data(data)
+      .enter().append("circle")
+        .attr("r", 5)
+        .attr("cx", function(d) { return x(d.age); })
+        .attr("cy", function(d) { return y(d.count); });
+
+    // add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the Y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+  // });
+};
+
+//BRAIN ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+export var brain = function() {
+  // findAgeRange();
+  let ageBreakdown = breakdownAgeCount();
+  let totalAgeCount = countTotalAgesNum(ageBreakdown);
+
+  let agePercentages = ageByPercentage(ageBreakdown, totalAgeCount);
+  let agePercentageRange = ageRangePercentage(20, 30, agePercentages);
+
+  let ageBreakExclNaN = excludeNaN(ageBreakdown);
+    
+  draw(ageBreakExclNaN);
+};
+
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { D3Service, D3, Selection } from 'd3-ng2-service';
 
+import { FirebaseListObservable } from 'angularfire2/database';
+import { PassengerService } from '../passenger.service';
+
+import * as calc from '../calc.js';
+
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.css']
+  styleUrls: ['./graph.component.css'],
+  providers: [PassengerService]
 })
 export class GraphComponent implements OnInit {
 
   private d3: D3;
   private parentNativeElement: any;
 
-  constructor(element: ElementRef, d3Service: D3Service) { 
+  constructor(element: ElementRef, d3Service: D3Service, private passengerService: PassengerService) { 
     this.d3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 Service
     this.parentNativeElement = element.nativeElement;
   }
@@ -26,6 +32,8 @@ export class GraphComponent implements OnInit {
 
       // Do d3 stuff
     }
+
+    
   }
 
 
